a82dc07e919afc51ea392802cf956e40a330ca77
Toonrix <Toonrix@github.com>
2024-03-30 18:38:29 +0100

    firt commit

---
+++ bruh.c
@@ -0,0 +1,11 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (int argc, char* argv[]) {
+
+    printf("Hello World!\n");
+    printf("Goodbye Mars!\n");
+
+    return 0;
+}
